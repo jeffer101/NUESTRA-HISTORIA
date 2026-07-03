f0c157f8aab9f2b01523b8984b7f6eeb9d912e73
Encuesta1103 <maldonadoyefer656@gmail.com>
2026-07-02 21:33:12 -0500

    Add event listener to show carta on button click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const boton = document.getElementById("entrar");
+const inicio = document.querySelector(".inicio");
+const carta = document.querySelector(".carta");
 
+boton.addEventListener("click", () => {
+    inicio.style.display = "none";
+    carta.style.display = "block";
+});
